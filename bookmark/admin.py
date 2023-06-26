890133e1f459a4ba0900b45b3548fd9ebad19f2e
Juwon Hwang <wndnjs719@gmail.com>
2023-06-26 11:14:49 +0900

    Django 실습 - admin 모델, 마이그레이션 추가

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from bookmark.models import Bookmark
+
+
 # Register your models here.
+
+# 데코레이터 패턴 사용하지 않는 방법
+# admin.site.register(Bookmark)
+@admin.register(Bookmark)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
