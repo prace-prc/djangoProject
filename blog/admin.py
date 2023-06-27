95590298b97814a76635c2bd4efe72222d9d534d
Juwon Hwang <wndnjs719@gmail.com>
2023-06-27 13:41:27 +0900

    Django 실습 - taggit 설정

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,13 @@ from blog.models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'modify_dt')
+    list_display = ('id', 'title', 'modify_dt', 'tag_list')
     list_filter = ('modify_dt',)
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title', )}
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, post):
+        return ','.join(tag.name for tag in post.tags.all())
