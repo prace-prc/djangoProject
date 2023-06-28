5b9aa39769277740f899de108bc02169245471f0
Juwon Hwang <wndnjs719@gmail.com>
2023-06-28 15:45:50 +0900

    Django 실습 - album_list.html 작성, url, field, view 내용 추가

--- photo/urls.py
+++ photo/urls.py
@@ -5,4 +5,6 @@ app_name = 'photo'
 
 urlpatterns = [
     path('', views.AlbumLV.as_view(), name='index'),
+    path('album/<int:pk>/', views.AlbumDV.as_view(), name='album_detail'),
+    path('photo/<int:pk>/', views.PhotoDV.as_view(), name='photo_detail'),
 ]
