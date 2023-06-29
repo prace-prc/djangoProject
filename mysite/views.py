147276a8111913b4d4f176d4e5a1cbb8ad153cd4
Juwon Hwang <wndnjs719@gmail.com>
2023-06-29 11:24:05 +0900

    Django 실습 - 인증용 설정 추가

--- mysite/views.py
+++ mysite/views.py
@@ -6,10 +6,12 @@ from django.views.generic import TemplateView, CreateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+
 class UserCreateView(CreateView):
     template_name = 'registration/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('register_done')
+
 
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html'
