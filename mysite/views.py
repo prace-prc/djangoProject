9f7ae5c0cffb616caf5a7b7e747f615563d172d0
Juwon Hwang <wndnjs719@gmail.com>
2023-07-04 09:46:36 +0900

    Django 실습 - mysite 뷰로직 수정

--- mysite/views.py
+++ mysite/views.py
@@ -17,5 +17,13 @@ class UserCreateView(CreateView):
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html'
 
+
 class OwnerOnlyMixin(AccessMixin):
-    pass
+    raise_exception = True
+    permission_denied_message = 'Owner only can update/delete the object'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if request.user != obj.owner:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
